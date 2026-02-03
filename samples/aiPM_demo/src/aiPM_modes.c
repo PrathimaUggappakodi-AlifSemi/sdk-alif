c3dae3c2e40aea2311521b2319fdd1fdd57afead
Prathima Uggappakodi <prathima.uggappakodi@alifsemi.com>
2026-02-03 11:33:42 -0800

    Committing the aiPM_modes C file

--- samples/aiPM_demo/src/aiPM_modes.c
+++ samples/aiPM_demo/src/aiPM_modes.c
@@ -26,6 +26,17 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(pm_system_off, LOG_LEVEL_INF);
 
+#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(rtc0), snps_dw_apb_rtc, okay)
+	#define WAKEUP_SOURCE DT_NODELABEL(rtc0)
+	#define SE_OFFP_EWIC_CFG EWIC_RTC_A
+	#define SE_OFFP_WAKEUP_EVENTS WE_LPRTC
+#elif DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(timer0), snps_dw_timers, okay)
+	#define WAKEUP_SOURCE DT_NODELABEL(timer0)
+	#define SE_OFFP_EWIC_CFG EWIC_VBAT_TIMER
+	#define SE_OFFP_WAKEUP_EVENTS WE_LPTIMER0
+#else
+#error "Wakeup Device not enabled in the dts"
+#endif
 /**
  * As per the application requirements, it can remove the memory blocks which are not in use.
  */
@@ -39,17 +50,9 @@ LOG_MODULE_REGISTER(pm_system_off, LOG_LEVEL_INF);
 	#define SERAM_MEMORY_BLOCKS_IN_USE SERAM_MASK
 #endif
 
-#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(rtc0), snps_dw_apb_rtc, okay)
-	#define WAKEUP_SOURCE DT_NODELABEL(rtc0)
-	#define SE_OFFP_EWIC_CFG EWIC_RTC_A
-	#define SE_OFFP_WAKEUP_EVENTS WE_LPRTC
-#elif DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(timer0), snps_dw_timers, okay)
-	#define WAKEUP_SOURCE DT_NODELABEL(timer0)
-	#define SE_OFFP_EWIC_CFG EWIC_VBAT_TIMER
-	#define SE_OFFP_WAKEUP_EVENTS WE_LPTIMER0
-#else
-#error "Wakeup Device not enabled in the dts"
-#endif
+
+#define HOST_BASE_SYS_CTRL		0x1A010000
+#define HOST_BSYS_PWR_REQ		(HOST_BASE_SYS_CTRL + 0x400)
 
 /* Sleep duration for PM_STATE_RUNTIME_IDLE */
 #define RUNTIME_IDLE_SLEEP_USEC (18 * 1000 * 1000)
@@ -74,51 +77,173 @@ LOG_MODULE_REGISTER(pm_system_off, LOG_LEVEL_INF);
  */
 #define IS_BOOTING_FROM_MRAM() (SCB->VTOR >= MRAM_BASE_ADDRESS)
 
-/*
- * PM_STATE_SUSPEND_TO_RAM (S2RAM) support:
- * - HP core: NOT supported (no retention capability)
- * - HE core + TCM boot: SUPPORTED (TCM retention keeps code and context)
- */
-#if defined(CONFIG_RTSS_HE)
-#define S2RAM_SUPPORTED (!IS_BOOTING_FROM_MRAM())
-#else
+
 #define S2RAM_SUPPORTED 0
+
+#define SOFT_OFF_SUPPORTED 1
+
+ #include <zephyr/pm/pm.h>
+
+/* PM notifier forward declarations */
+static void pm_notify_state_entry(enum pm_state state);
+static void pm_notify_pre_device_resume(enum pm_state state);
+static void pm_notify_state_exit(enum pm_state state);
+
+ 
+ /*
+ * This function will be invoked in the PRE_KERNEL_2 phase of the init routine.
+ */
+ int app_set_stop1_params()
+{
+        off_profile_t offp;
+        int ret;
+        uint32_t regdata;
+        
+        offp.ewic_cfg = SE_OFFP_EWIC_CFG;
+	offp.wakeup_events = SE_OFFP_WAKEUP_EVENTS;
+	offp.vtor_address = SCB->VTOR;
+	offp.vtor_address_ns = SCB->VTOR;
+	offp.stby_clk_src      = CLK_SRC_HFRC;
+        offp.stby_clk_freq     = SCALED_FREQ_RC_ACTIVE_76_8_MHZ;
+        offp.power_domains     = 0;    // no power domains will request STOP Mode
+        offp.dcdc_mode         = DCDC_MODE_PWM;
+        offp.dcdc_voltage      = DCDC_VOUT_0825;
+        offp.vdd_ioflex_3V3    = IOFLEX_LEVEL_1V8;
+        offp.aon_clk_src       = CLK_SRC_LFXO;
+        offp.memory_blocks     = SRAM4_1_MASK | SRAM4_2_MASK | SRAM5_1_MASK | SRAM5_2_MASK | BACKUP4K_MASK;
+        
+        //BOD-enable
+        regdata = sys_read32((ANA_BASE + 0x40));
+	regdata |= (1U << 8);
+	sys_write32(regdata, (ANA_BASE + 0x40));
+	
+	//enable LPRTC
+	regdata = sys_read32((VBAT_BASE + 0x10));
+	regdata |= 0x1;
+	sys_write32(regdata, (VBAT_BASE + 0x10));
+        
+        ret = se_service_set_off_cfg(&offp);
+	__ASSERT(ret == 0, "SE: set_off_cfg failed = %d", ret);
+	
+	return ret;
+
+}
+ 
+int app_set_go1_params(void)
+{
+	run_profile_t runp;
+	int ret;
+
+	runp.power_domains = PD_SYST_MASK | PD_SESS_MASK;
+	runp.dcdc_voltage  = 825;
+	runp.dcdc_mode     = DCDC_MODE_PWM;
+	runp.aon_clk_src   = CLK_SRC_LFXO;
+	runp.run_clk_src   = CLK_SRC_PLL;
+	runp.scaled_clk_freq = SCALED_FREQ_RC_ACTIVE_76_8_MHZ;
+	runp.vdd_ioflex_3V3 = IOFLEX_LEVEL_1V8;
+	runp.ip_clock_gating = 0;
+	runp.phy_pwr_gating = 0;
+#if defined(CONFIG_RTSS_HP)
+	runp.cpu_clk_freq  = CLOCK_FREQUENCY_400MHZ;
+#else
+	runp.cpu_clk_freq  = CLOCK_FREQUENCY_160MHZ;
 #endif
 
-#define START_WITH_HFRC 1
-/*
- * PM_STATE_SOFT_OFF support:
- * - HP core: Always supported (no retention, must use SOFT_OFF)
- * - HE core + MRAM boot: Supported (MRAM preserved, wakeup possible)
- * - HE core + TCM boot: Skip (use S2RAM with retention instead)
- */
-//#if defined(CONFIG_RTSS_HP)
-#define SOFT_OFF_SUPPORTED 1
-//#elif defined(CONFIG_RTSS_HE)
-//#define SOFT_OFF_SUPPORTED IS_BOOTING_FROM_MRAM()
-//#else
-//#define SOFT_OFF_SUPPORTED 1
-//#endif
+	runp.memory_blocks =  SRAM2_MASK | SRAM3_MASK | MRAM_MASK;
 
-#define OFF_STATE_NODE_ID DT_PHANDLE_BY_IDX(DT_NODELABEL(cpu0), cpu_power_states, 0)
+	ret = se_service_set_run_cfg(&runp);
+	__ASSERT(ret == 0, "SE: set_run_cfg failed = %d", ret);
 
-//BUILD_ASSERT((RUNTIME_IDLE_SLEEP_USEC < DT_PROP_OR(OFF_STATE_NODE_ID, min_residency_us, 0)),
-//	"RUNTIME_IDLE sleep should be less than min-residency-us");
+	return ret;
+}
 
 #if 0
-#if defined(CONFIG_RTSS_HE)
-/* Additional validation for power state sleep durations */
-BUILD_ASSERT((S2RAM_STOP_SLEEP_USEC > S2RAM_STANDBY_SLEEP_USEC),
-	"STOP sleep duration should be greater than STANDBY sleep duration");
-BUILD_ASSERT((SOFT_OFF_SLEEP_USEC > S2RAM_STOP_SLEEP_USEC),
-	"SOFT_OFF sleep duration should be greater than STOP sleep duration");
-#endif
-#endif
+ static struct pm_notifier app_pm_notifier = {
+	.state_entry = pm_notify_state_entry,
+	.pre_device_resume = pm_notify_pre_device_resume,
+};
+
 
 /**
- * Helper function to lock/unlock deeper power states
- * @param lock true to lock deeper states (allow only RUNTIME_IDLE), false to unlock all
+ * PM Notifier callback for power state entry
  */
+static void pm_notify_state_entry(enum pm_state state)
+{
+	const struct pm_state_info *next_state = pm_state_next_get(0);
+	uint8_t substate_id = next_state ? next_state->substate_id : 0;
+	int ret;
+
+	switch (state) {
+	case PM_STATE_SUSPEND_TO_RAM:
+	//case PM_STATE_SOFT_OFF:
+		ret = app_set_stop1_params(state, substate_id);
+		__ASSERT(ret == 0, "app_set_off_params failed = %d", ret);
+		break;
+	default:
+		__ASSERT(false, "Entering unknown power state %d", state);
+		break;
+	}
+}
+
+static void pm_notify_pre_device_resume(enum pm_state state)
+{
+	int ret;
+
+	switch (state) {
+	case PM_STATE_SUSPEND_TO_RAM:
+		ret = app_set_go1_params();
+	//	__ASSERT(ret == 0, "app_set_run_params failed = %d", ret);
+		break;
+	//case PM_STATE_SOFT_OFF:
+		/* No action needed - SOFT_OFF causes reset, not resume */
+	//	break;
+	default:
+		__ASSERT(false, "Pre-resume for unknown power state %d", state);
+		break;
+	}
+}
+
+
+
+
+
+
+static volatile uint32_t alarm_cb_status;
+static void alarm_callback_fn(const struct device *wakeup_dev,
+				uint8_t chan_id, uint32_t ticks,
+				void *user_data)
+{
+	LOG_DBG("%s: Alarm triggered", wakeup_dev->name);
+	alarm_cb_status = 1;
+}
+ 
+int app_enter_deep_sleep(uint32_t sleep_usec)
+{
+
+	const struct device *const wakeup_dev = DEVICE_DT_GET(WAKEUP_SOURCE);
+	struct counter_alarm_cfg alarm_cfg;
+	int ret;
+	/*
+	 * Set the alarm and delay so that idle thread can run
+	 */
+	alarm_cfg.ticks = counter_us_to_ticks(wakeup_dev, sleep_usec);
+	ret = counter_set_channel_alarm(wakeup_dev, 0, &alarm_cfg);
+	if (ret) {
+		LOG_ERR("Failed to set the alarm (err %d)", ret);
+		return ret;
+	}
+
+	LOG_DBG("Set alarm for %u microseconds", sleep_usec);
+	/*
+	 * Wait for the alarm to trigger. The idle thread will
+	 * take care of entering the deep sleep state via PM framework.
+	 */
+	k_sleep(K_USEC(sleep_usec));
+
+        return 0;
+}
+#endif
+
 void app_pm_lock_deeper_states(bool lock)
 {
 	const char *state_desc;
@@ -175,12 +300,26 @@ void app_pm_lock_deeper_states(bool lock)
 	       lock ? "Locked" : "Unlocked", state_desc);
 }
 
+/*
+static int app_pre_kernel_init(void)
+{
+	/* Lock deeper power states to allow only RUNTIME_IDLE */
+	//app_pm_lock_deeper_states(true);
+
+	/* Register PM notifier callbacks */
+/*	pm_notifier_register(&app_pm_notifier);
+
+	return 0;
+}
+SYS_INIT(app_pre_kernel_init, PRE_KERNEL_2, 0);
+
+
 void app_pm_unlock_deeper_states(uint32_t period_ms)
 {
 	k_sleep(K_MSEC(period_ms));
 	pm_policy_state_lock_put(PM_STATE_SUSPEND_TO_RAM, PM_ALL_SUBSTATES);
 }
-
+*/
 
 #if defined(CONFIG_START_WITH_DIVIDED_HFRC)
 
@@ -356,34 +495,6 @@ int app_set_ready2_params(void)
 
 #else
 
-int app_set_go1_params(void)
-{
-	run_profile_t runp;
-	int ret;
-
-	runp.power_domains = PD_SYST_MASK | PD_SESS_MASK;
-	runp.dcdc_voltage  = 825;
-	runp.dcdc_mode     = DCDC_MODE_PWM;
-	runp.aon_clk_src   = CLK_SRC_LFXO;
-	runp.run_clk_src   = CLK_SRC_PLL;
-	runp.scaled_clk_freq = SCALED_FREQ_RC_ACTIVE_76_8_MHZ;
-	runp.vdd_ioflex_3V3 = IOFLEX_LEVEL_1V8;
-	runp.ip_clock_gating = 0;
-	runp.phy_pwr_gating = 0;
-#if defined(CONFIG_RTSS_HP)
-	runp.cpu_clk_freq  = CLOCK_FREQUENCY_400MHZ;
-#else
-	runp.cpu_clk_freq  = CLOCK_FREQUENCY_160MHZ;
-#endif
-
-	runp.memory_blocks =  SRAM2_MASK | SRAM3_MASK | MRAM_MASK;
-
-	ret = se_service_set_run_cfg(&runp);
-	__ASSERT(ret == 0, "SE: set_run_cfg failed = %d", ret);
-
-	return ret;
-}
-
 int app_set_ready1_systop_on_params(void)
 {
 	run_profile_t runp;
@@ -416,6 +527,7 @@ int app_set_ready1_systop_off_params(void)
 {
 	run_profile_t runp;
 	int ret;
+	uint32_t regdata;
 
 	runp.power_domains = PD_SSE700_AON_MASK ;
 	runp.dcdc_voltage  = 825;
@@ -436,6 +548,11 @@ int app_set_ready1_systop_off_params(void)
 
 	ret = se_service_set_run_cfg(&runp);
 	__ASSERT(ret == 0, "SE: set_run_cfg failed = %d", ret);
+	
+	//disabling the systop in host reg
+	regdata = sys_read32(HOST_BSYS_PWR_REQ);
+	regdata &= 0;
+	sys_write32(regdata, (HOST_BSYS_PWR_REQ));
 
 	return ret;
 }
@@ -468,41 +585,7 @@ int app_set_go3_params(void)
 	return ret;
 }
 
-int app_set_stop1_params()
-{
-        off_profile_t offp;
-        int ret;
-        uint32_t regdata;
-        
-        offp.ewic_cfg = 0;
-	offp.wakeup_events = 0;
-	offp.vtor_address = SCB->VTOR;
-	offp.vtor_address_ns = SCB->VTOR;
-	offp.stby_clk_src      = CLK_SRC_HFRC;
-        offp.stby_clk_freq     = SCALED_FREQ_RC_ACTIVE_76_8_MHZ;
-        offp.power_domains     = 0;    // no power domains will request STOP Mode
-        offp.dcdc_mode         = DCDC_MODE_PWM;
-        offp.dcdc_voltage      = DCDC_VOUT_0825;
-        offp.vdd_ioflex_3V3    = IOFLEX_LEVEL_1V8;
-        offp.aon_clk_src       = CLK_SRC_LFXO;
-        offp.memory_blocks     = SRAM4_1_MASK | SRAM4_2_MASK | SRAM5_1_MASK | SRAM5_2_MASK | BACKUP4K_MASK;
-        
-        //BOD-enable
-        regdata = sys_read32((ANA_BASE + 0x40));
-	regdata |= (1U << 8);
-	sys_write32(regdata, (ANA_BASE + 0x40));
-	
-	//enable LPRTC
-	regdata = sys_read32((VBAT_REG_BASE + 0x10));
-	regdata |= 0x1;
-	sys_write32(regdata, (VBAT_REG_BASE + 0x10));
-        
-        ret = se_service_set_off_cfg(&offp);
-	__ASSERT(ret == 0, "SE: set_off_cfg failed = %d", ret);
-	
-	return ret;
 
-}
 
 
 int app_set_stop2_params()
@@ -530,9 +613,9 @@ int app_set_stop2_params()
 	sys_write32(regdata, (ANA_BASE + 0x40));
 	
 	//enable LPRTC
-	regdata = sys_read32((VBAT_REG_BASE + 0x10));
+	regdata = sys_read32((VBAT_BASE + 0x10));
 	regdata |= 0x1;
-	sys_write32(regdata, (VBAT_REG_BASE + 0x10));
+	sys_write32(regdata, (VBAT_BASE + 0x10));
         
         ret = se_service_set_off_cfg(&offp);
 	__ASSERT(ret == 0, "SE: set_off_cfg failed = %d", ret);
@@ -568,9 +651,9 @@ int app_set_stop3_params()
 	sys_write32(regdata, (ANA_BASE + 0x40));
 	
 	//enable LPRTC
-	regdata = sys_read32((VBAT_REG_BASE + 0x10));
+	regdata = sys_read32((VBAT_BASE + 0x10));
 	regdata |= 0x1;
-	sys_write32(regdata, (VBAT_REG_BASE + 0x10));
+	sys_write32(regdata, (VBAT_BASE + 0x10));
         
         ret = se_service_set_off_cfg(&offp);
 	__ASSERT(ret == 0, "SE: set_off_cfg failed = %d", ret);
@@ -604,9 +687,9 @@ int app_set_stop4_params(void)
 	sys_write32(regdata, (ANA_BASE + 0x40));
 	
 	//enable LPRTC
-	regdata = sys_read32((VBAT_REG_BASE + 0x10));
+	regdata = sys_read32((VBAT_BASE + 0x10));
 	regdata |= 0x1;
-	sys_write32(regdata, (VBAT_REG_BASE + 0x10));
+	sys_write32(regdata, (VBAT_BASE + 0x10));
         
         ret = se_service_set_off_cfg(&offp);
 	__ASSERT(ret == 0, "SE: set_off_cfg failed = %d", ret);
@@ -642,9 +725,9 @@ int app_set_stop5_params(void)
 	sys_write32(regdata, (ANA_BASE + 0x40));
 	
 	//disable LPRTC
-	regdata = sys_read32((VBAT_REG_BASE + 0x10));
+	regdata = sys_read32((VBAT_BASE + 0x10));
 	regdata &= ~(0x1);
-	sys_write32(regdata, (VBAT_REG_BASE + 0x10));
+	sys_write32(regdata, (VBAT_BASE + 0x10));
         
         ret = se_service_set_off_cfg(&offp);
 	__ASSERT(ret == 0, "SE: set_off_cfg failed = %d", ret);
