0478052e180ef32b9ee32e14f99129c743331af4
Prathima Uggappakodi <prathima.uggappakodi@alifsemi.com>
2026-02-03 11:34:11 -0800

    Committing the shell CLI file

--- samples/aiPM_demo/src/aiPM_shell.c
+++ samples/aiPM_demo/src/aiPM_shell.c
@@ -13,7 +13,6 @@
 #include <zephyr/pm/pm.h>
 #include <zephyr/pm/policy.h>
 #if defined(CONFIG_PM)
-#include <power_mgr.h>
 #include "aiPM_modes.h"
 #endif
 #include <es0_power_manager.h>
@@ -27,100 +26,22 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(LOG_MODULE_NAME);
 
-#define NVD_BOOT_PARAMS_MAX_SIZE (512)
 
-#define LL_CLK_SEL_CTRL_REG_ADDR   0x1A60201C
-#define LL_UART_CLK_SEL_CTRL_16MHZ 0x00
-#define LL_UART_CLK_SEL_CTRL_24MHZ 0x01
-#define LL_UART_CLK_SEL_CTRL_48MHZ 0x03
+#define BK_RAM_BASE 0x4902C000U
+#define BK_RAM_SIZE 1000U
+#define POWER_MODE_OFFSET 1
+#define HP_START_OFFSET 0
 
-/* Tag status: (STATUS_VALID | STATUS_NOT_LOCKED | STATUS_NOT_ERASED) */
-#define DEFAULT_TAG_STATUS (0x00 | 0x02 | 0x04)
-
-/* Boot time value definitions */
-#define BOOT_PARAM_ID_LE_CODED_PHY_500          0x85
-#define BOOT_PARAM_ID_DFT_SLAVE_MD              0x20
-#define BOOT_PARAM_ID_CH_CLASS_REP_INTV         0x36
-#define BOOT_PARAM_ID_BD_ADDRESS                0x01
-#define BOOT_PARAM_ID_ACTIVITY_MOVE_CONFIG      0x15
-#define BOOT_PARAM_ID_SCAN_EXT_ADV              0x16
-#define BOOT_PARAM_ID_RSSI_HIGH_THR             0x3A
-#define BOOT_PARAM_ID_RSSI_LOW_THR              0x3B
-#define BOOT_PARAM_ID_SLEEP_ENABLE              0x11
-#define BOOT_PARAM_ID_EXT_WAKEUP_ENABLE         0x12
-#define BOOT_PARAM_ID_ENABLE_CHANNEL_ASSESSMENT 0x19
-#define BOOT_PARAM_ID_RSSI_INTERF_THR           0x3C
-#define BOOT_PARAM_ID_UART_BAUDRATE             0x10
-#define BOOT_PARAM_ID_UART_INPUT_CLK_FREQ       0xC0
-#define BOOT_PARAM_ID_NO_PARAM                  0xFF
-#define BOOT_PARAM_ID_EXT_WAKEUP_TIME           0x0D
-#define BOOT_PARAM_ID_OSC_WAKEUP_TIME           0x0E
-#define BOOT_PARAM_ID_RM_WAKEUP_TIME            0x0F
-#define BOOT_PARAM_ID_EXT_WARMBOOT_WAKEUP_TIME  0xD0
-#define BOOT_PARAM_ID_LPCLK_DRIFT               0x07
-#define BOOT_PARAM_ID_ACTCLK_DRIFT              0x09
-#define BOOT_PARAM_ID_CONFIGURATION             0xD1
-
-#define BOOT_PARAM_LEN_LE_CODED_PHY_500          1
-#define BOOT_PARAM_LEN_DFT_SLAVE_MD              1
-#define BOOT_PARAM_LEN_CH_CLASS_REP_INTV         2
-#define BOOT_PARAM_LEN_BD_ADDRESS                6
-#define BOOT_PARAM_LEN_ACTIVITY_MOVE_CONFIG      1
-#define BOOT_PARAM_LEN_SCAN_EXT_ADV              1
-#define BOOT_PARAM_LEN_RSSI_THR                  1
-#define BOOT_PARAM_LEN_SLEEP_ENABLE              1
-#define BOOT_PARAM_LEN_EXT_WAKEUP_ENABLE         1
-#define BOOT_PARAM_LEN_ENABLE_CHANNEL_ASSESSMENT 1
-#define BOOT_PARAM_LEN_UART_BAUDRATE             4
-#define BOOT_PARAM_LEN_UART_INPUT_CLK_FREQ       4
-#define BOOT_PARAM_LEN_EXT_WAKEUP_TIME           2
-#define BOOT_PARAM_LEN_OSC_WAKEUP_TIME           2
-#define BOOT_PARAM_LEN_RM_WAKEUP_TIME            2
-#define BOOT_PARAM_LEN_EXT_WARMBOOT_WAKEUP_TIME  2
-#define BOOT_PARAM_LEN_LPCLK_DRIFT               2
-#define BOOT_PARAM_LEN_ACTCLK_DRIFT              1
-#define BOOT_PARAM_LEN_CONFIGURATION             4
-
-#define ES0_PM_ERROR_NO_ERROR             0
-#define ES0_PM_ERROR_TOO_MANY_USERS       -1
-#define ES0_PM_ERROR_TOO_MANY_BOOT_PARAMS -2
-#define ES0_PM_ERROR_INVALID_BOOT_PARAMS  -3
-#define ES0_PM_ERROR_START_FAILED         -4
-#define ES0_PM_ERROR_NO_BAUDRATE          -5
-#define ES0_PM_ERROR_BAUDRATE_MISMATCH    -6
-
-/* LP timer callback status */
-static volatile int lp_timer_fired = 0;
-
-/* Zephyr timer for low-power duration */
-K_TIMER_DEFINE(lp_timer, NULL, NULL);
-
+static int32_t bk_ram_wr(uint32_t *data, uint32_t offset)
+{
+    if (offset < BK_RAM_SIZE) {
+        sys_write32(*data, BK_RAM_BASE + offset * sizeof(uint32_t));
+        return 0;
+    }
+    return -1;
+}
 
 #if defined(CONFIG_PM)
-static uint32_t wakeup_counter __attribute__((noinit));
-static pm_state_mode_type_e pm_off_mode __attribute__((noinit));
-
-
-static int pm_application_init(void)
-{
-	if (power_mgr_cold_boot()) {
-		wakeup_counter = 0;
-	} else {
-		if (wakeup_counter) {
-			/* Set a off profile */
-			if (power_mgr_set_offprofile(pm_off_mode)) {
-				printk("Error to set off profile\n");
-				wakeup_counter = 0;
-				return 0;
-			}
-			wakeup_counter--;
-			//power_mgr_ready_for_sleep();
-			//power_mgr_set_subsys_off_period(sleep_period);
-		}
-	}
-	return 0;
-}
-SYS_INIT(pm_application_init, APPLICATION, 1);
 
 
 static int cmd_standby_test(const struct shell *shell, size_t argc, char **argv)
@@ -134,7 +55,6 @@ static int cmd_standby_test(const struct shell *shell, size_t argc, char **argv)
 
 	if (ret) {
 		shell_fprintf(shell, SHELL_VT100_COLOR_DEFAULT, "ERROR: %d\n", ret);
-		//wakeup_counter = 0;
 		return ret;
 	}
 
@@ -154,7 +74,6 @@ static int cmd_ready1_systop_test(const struct shell *shell, size_t argc, char *
 
 	if (ret) {
 		shell_fprintf(shell, SHELL_VT100_COLOR_DEFAULT, "ERROR: %d\n", ret);
-		//wakeup_counter = 0;
 		return ret;
 	}
 
@@ -165,7 +84,7 @@ static int cmd_ready1_systop_test(const struct shell *shell, size_t argc, char *
 static int cmd_ready1_systop_off_test(const struct shell *shell, size_t argc, char **argv)
 {
 
-	shell_fprintf(shell, SHELL_VT100_COLOR_DEFAULT, "Start Ready 1 mode with SYSTOP on\n");
+	shell_fprintf(shell, SHELL_VT100_COLOR_DEFAULT, "Start Ready 1 mode with SYSTOP off\n");
 	
         int ret = app_set_ready1_systop_off_params();
         app_pm_lock_deeper_states(true);
@@ -175,7 +94,6 @@ static int cmd_ready1_systop_off_test(const struct shell *shell, size_t argc, ch
 
 	if (ret) {
 		shell_fprintf(shell, SHELL_VT100_COLOR_DEFAULT, "ERROR: %d\n", ret);
-		//wakeup_counter = 0;
 		return ret;
 	}
 
@@ -192,7 +110,6 @@ static int cmd_go1_test(const struct shell *shell, size_t argc, char **argv)
 
 	if (ret) {
 		shell_fprintf(shell, SHELL_VT100_COLOR_DEFAULT, "ERROR: %d\n", ret);
-		//wakeup_counter = 0;
 		return ret;
 	}
 
@@ -208,7 +125,6 @@ static int cmd_go3_test(const struct shell *shell, size_t argc, char **argv)
 
 	if (ret) {
 		shell_fprintf(shell, SHELL_VT100_COLOR_DEFAULT, "ERROR: %d\n", ret);
-		//wakeup_counter = 0;
 		return ret;
 	}
 
@@ -224,7 +140,6 @@ static int cmd_go4_test(const struct shell *shell, size_t argc, char **argv)
 
 	if (ret) {
 		shell_fprintf(shell, SHELL_VT100_COLOR_DEFAULT, "ERROR: %d\n", ret);
-		//wakeup_counter = 0;
 		return ret;
 	}
 
@@ -244,7 +159,6 @@ static int cmd_ready2_test(const struct shell *shell, size_t argc, char **argv)
 
 	if (ret) {
 		shell_fprintf(shell, SHELL_VT100_COLOR_DEFAULT, "ERROR: %d\n", ret);
-		//wakeup_counter = 0;
 		return ret;
 	}
 
@@ -254,9 +168,13 @@ static int cmd_stop1_test(const struct shell *shell, size_t argc, char **argv)
 {
 
 	shell_fprintf(shell, SHELL_VT100_COLOR_DEFAULT, "Start STOP 1 mode \n");
-	
+
         int ret = app_set_stop1_params();
         app_pm_lock_deeper_states(false);
+        //app_enter_deep_sleep(22000000);
+        k_sleep(K_USEC(22000000));
+        	
+	//LOG_INF("=== Resumed from PM_STATE_SUSPEND_TO_RAM (substate 1: STOP) ===");
 
 	if (ret) {
 		shell_fprintf(shell, SHELL_VT100_COLOR_DEFAULT, "ERROR: %d\n", ret);
@@ -273,6 +191,8 @@ static int cmd_stop2_test(const struct shell *shell, size_t argc, char **argv)
 	
         int ret = app_set_stop2_params();
         app_pm_lock_deeper_states(false);
+        k_sleep(K_USEC(22000000));
+       // app_enter_deep_sleep(22000000);
 
 	if (ret) {
 		shell_fprintf(shell, SHELL_VT100_COLOR_DEFAULT, "ERROR: %d\n", ret);
@@ -289,6 +209,7 @@ static int cmd_stop3_test(const struct shell *shell, size_t argc, char **argv)
 	
         int ret = app_set_stop3_params();
         app_pm_lock_deeper_states(false);
+        k_sleep(K_USEC(22000000));
 
 	if (ret) {
 		shell_fprintf(shell, SHELL_VT100_COLOR_DEFAULT, "ERROR: %d\n", ret);
@@ -305,6 +226,7 @@ static int cmd_stop4_test(const struct shell *shell, size_t argc, char **argv)
 	
         int ret = app_set_stop4_params();
         app_pm_lock_deeper_states(false);
+        k_sleep(K_USEC(22000000));
 
 	if (ret) {
 		shell_fprintf(shell, SHELL_VT100_COLOR_DEFAULT, "ERROR: %d\n", ret);
@@ -321,6 +243,7 @@ static int cmd_stop5_test(const struct shell *shell, size_t argc, char **argv)
 	
         int ret = app_set_stop5_params();
         app_pm_lock_deeper_states(false);
+        k_sleep(K_USEC(27000000));
 
 	if (ret) {
 		shell_fprintf(shell, SHELL_VT100_COLOR_DEFAULT, "ERROR: %d\n", ret);
@@ -369,7 +292,7 @@ SHELL_STATIC_SUBCMD_SET_CREATE(
 
 
 #endif
-	//SHELL_CMD_ARG(stop, NULL, "es0 stop", cmd_stop, 1, 10),
+
 
 	SHELL_SUBCMD_SET_END);
 
