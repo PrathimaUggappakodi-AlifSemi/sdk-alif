07cf7598030d9e4b7c70a904ec82e5c4969b550a
Prathima Uggappakodi <prathima.uggappakodi@alifsemi.com>
2026-02-18 14:49:21 -0800

    Committing the aiPM_shell.c file

--- samples/aiPM_demo/src/aiPM_shell.c
+++ samples/aiPM_demo/src/aiPM_shell.c
@@ -20,7 +20,7 @@
 #include <stdlib.h>
 #include <inttypes.h>
 
-#define LOG_MODULE_NAME alif_power_mgr_shell
+//#define LOG_MODULE_NAME alif_power_mgr_shell
 #define LOG_LEVEL       LOG_LEVEL_INFO
 
 #include <zephyr/logging/log.h>
@@ -29,16 +29,38 @@ LOG_MODULE_REGISTER(LOG_MODULE_NAME);
 
 #define BK_RAM_BASE 0x4902C000U
 #define BK_RAM_SIZE 1000U
-#define POWER_MODE_OFFSET 1
-#define HP_START_OFFSET 0
+#define POWER_MODE_OFFSET 0
+#define HP_START_OFFSET 1
+
+uint32_t power_mode_input=0;
+uint32_t hp_start_flag=0;
 
 static int32_t bk_ram_wr(uint32_t *data, uint32_t offset)
 {
     if (offset < BK_RAM_SIZE) {
-        sys_write32(*data, BK_RAM_BASE + offset * sizeof(uint32_t));
+        sys_write32(*data, BK_RAM_BASE + (offset * sizeof(uint32_t)));
         return 0;
     }
     return -1;
+}
+
+static void backup_ram_write(uint32_t power_mode)
+{
+     while(1)
+     {
+          if( (bk_ram_wr(&power_mode,POWER_MODE_OFFSET)!= -1))
+          {
+             break;
+          }
+     }
+
+     while(1)
+     {
+          hp_start_flag=1;
+          if((bk_ram_wr(&hp_start_flag,HP_START_OFFSET))!= -1)
+          break;
+     }
+     
 }
 
 #if defined(CONFIG_PM)
@@ -50,7 +72,6 @@ static int cmd_standby_test(const struct shell *shell, size_t argc, char **argv)
 	
         int ret = app_set_standby_params();
         app_pm_lock_deeper_states(true);
-        app_pm_unlock_deeper_states(1000);
         shell_fprintf(shell, SHELL_VT100_COLOR_DEFAULT, "Standby state set \n");
 
 	if (ret) {
@@ -168,13 +189,9 @@ static int cmd_stop1_test(const struct shell *shell, size_t argc, char **argv)
 {
 
 	shell_fprintf(shell, SHELL_VT100_COLOR_DEFAULT, "Start STOP 1 mode \n");
-
         int ret = app_set_stop1_params();
         app_pm_lock_deeper_states(false);
-        //app_enter_deep_sleep(22000000);
         k_sleep(K_USEC(22000000));
-        	
-	//LOG_INF("=== Resumed from PM_STATE_SUSPEND_TO_RAM (substate 1: STOP) ===");
 
 	if (ret) {
 		shell_fprintf(shell, SHELL_VT100_COLOR_DEFAULT, "ERROR: %d\n", ret);
@@ -192,7 +209,6 @@ static int cmd_stop2_test(const struct shell *shell, size_t argc, char **argv)
         int ret = app_set_stop2_params();
         app_pm_lock_deeper_states(false);
         k_sleep(K_USEC(22000000));
-       // app_enter_deep_sleep(22000000);
 
 	if (ret) {
 		shell_fprintf(shell, SHELL_VT100_COLOR_DEFAULT, "ERROR: %d\n", ret);
@@ -238,12 +254,16 @@ static int cmd_stop4_test(const struct shell *shell, size_t argc, char **argv)
 
 static int cmd_stop5_test(const struct shell *shell, size_t argc, char **argv)
 {
+	uint32_t regdata1=12, regdata2=1;
 
 	shell_fprintf(shell, SHELL_VT100_COLOR_DEFAULT, "Start STOP 5 mode \n");
-	
+	//backup_ram_write(12);
+	// sys_write32(regdata1, (BK_RAM_BASE + 0x0));
+	// sys_write32(regdata2, (BK_RAM_BASE +0x10));
         int ret = app_set_stop5_params();
         app_pm_lock_deeper_states(false);
         k_sleep(K_USEC(27000000));
+
 
 	if (ret) {
 		shell_fprintf(shell, SHELL_VT100_COLOR_DEFAULT, "ERROR: %d\n", ret);
@@ -259,7 +279,7 @@ SHELL_STATIC_SUBCMD_SET_CREATE(
 	sub_cmds, /*SHELL_CMD_ARG(start, NULL, "es0 start", cmd_start, 1, 10),*/
 #if defined(CONFIG_PM)
         
-        #if !defined(CONFIG_START_WITH_HFRC)
+        #if !defined(CONFIG_START_WITH_HFRC) || !defined(CONFIG_START_WITH_DIVIDED_HFRC)
 	SHELL_CMD_ARG(pm_go1, NULL, "Start GO1-state ", cmd_go1_test, 1,
 		      10),
 	SHELL_CMD_ARG(pm_go3, NULL, "Start GO3-state ", cmd_go3_test, 1,
@@ -267,6 +287,8 @@ SHELL_STATIC_SUBCMD_SET_CREATE(
 	SHELL_CMD_ARG(pm_ready1_with_systop, NULL, "Start ready with systop on-state ", cmd_ready1_systop_test, 1,
 		      10),
 	SHELL_CMD_ARG(pm_ready1_with_off_systop, NULL, "Start ready with systop off-state ", cmd_ready1_systop_off_test, 1,
+		      10),
+	SHELL_CMD_ARG(pm_standby, NULL, "Start standby-state ", cmd_standby_test, 1,
 		      10),
 	SHELL_CMD_ARG(pm_stop1, NULL, "Start STOP1-state ", cmd_stop1_test, 1,
 		      10),
@@ -278,21 +300,16 @@ SHELL_STATIC_SUBCMD_SET_CREATE(
 		      10),
 	SHELL_CMD_ARG(pm_stop5, NULL, "Start STOP5-state ", cmd_stop5_test, 1,
 		      10), 
+	//Keeping the infrastructure for shell commands. Omce HFRC support for UART is available, these lines can be uncommented
 	#elif defined(CONFIG_START_WITH_HFRC)	
-	SHELL_CMD_ARG(pm_go4, NULL, "Start GO4-state ", cmd_go4_test, 1,
+	//SHELL_CMD_ARG(pm_go4, NULL, "Start GO4-state ", cmd_go4_test, 1,
 		      10),	
-	SHELL_CMD_ARG(pm_ready2, NULL, "Start Ready2-state ", cmd_ready2_test, 1,
+	//SHELL_CMD_ARG(pm_ready2, NULL, "Start Ready2-state ", cmd_ready2_test, 1,
 		      10),
         #endif
-		      
-	#if defined(CONFIG_START_WITH_DIVIDED_HFRC)
-	//SHELL_CMD_ARG(pm_standby, NULL, "Start standby-state ", cmd_standby_test, 1,
-	//	      10),
-	#endif
 
 
 #endif
-
 
 	SHELL_SUBCMD_SET_END);
 
